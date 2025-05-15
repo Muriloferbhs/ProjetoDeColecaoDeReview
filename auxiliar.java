d33412b722bb891be6af75bc8a41a4b5926b9512
Muriloferbhs <murfeg@gmail.com>
2025-05-15 08:37:26 -0300

    Create auxiliar.java

---
+++ auxiliar.java
@@ -0,0 +1,13 @@
+package com.mycompany.colecaoenari;
+
+
+public class Auxiliar {
+    
+    public String pessoa;
+    public String jogo;
+    public String cadastro;
+    public String recomendo;
+    public String review;
+    
+    
+}
